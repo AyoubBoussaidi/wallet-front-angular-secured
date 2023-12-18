270ce1b6dffcc963092faa67fdeeae11328eb415
Ayoub Boussaidi <ayoubboussaidi10@gmail.com>
2023-12-18 19:32:09 +0100

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+// @ts-ignore
 import { NgModule } from '@angular/core';
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
+// @ts-ignore
 import { HttpClientModule } from '@angular/common/http';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {CurrenciesComponent} from "./currencies/currencies.component";
@@ -11,7 +14,9 @@ import {ContinentsComponent} from "./continents/continents.component";
 import {WalletsComponent} from "./wallets/wallets.component";
 import {WalletTransactionsComponent} from "./wallet-transactions/wallet-transactions.component";
 import {CurrencyDepositComponent} from "./currency-deposit/currency-deposit.component";
+// @ts-ignore
 import {ReactiveFormsModule} from "@angular/forms";
+import {KeycloakAngularModule} from "keycloak-angular";
 
 @NgModule({
   declarations: [
@@ -28,7 +33,8 @@ import {ReactiveFormsModule} from "@angular/forms";
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    KeycloakAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
